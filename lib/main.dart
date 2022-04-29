64404df21605287f19c2a4f690250fa19cae6399
Mizuki Kubota <mizukick@users.noreply.github.com>
2022-04-30 03:43:17 +0900

    画面遷移を追加

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todoアプリ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -23,6 +23,32 @@ class TodoListPage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Text('リスト一覧画面'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              return TodoAddPage();
+            }),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TodoAddPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('リスト追加画面（クリックで戻る）'),
+        ),
       ),
     );
   }
